474d948db7b8e7594335d1838dbdfdacb08a1e7d
SoniEx2 <endermoneymod@gmail.com>
2020-07-27 22:00:08 -0300

    Fix setup.py
    Oops

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import setuptools
 
-setuptools.setup(packages=["gan385e734a52e13949a7a5c71827f6de920dbfea43"], install_requires=[
+setuptools.setup(name="gan385e734a52e13949a7a5c71827f6de920dbfea43", packages=["ganarchy"], install_requires=[
     "gan0f74bd87a23b515b45da7e6f5d9cc82380443dab",  # a boneless datastructure library
     "Click", "Jinja2", "qtoml", "requests"])
